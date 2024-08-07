c284d2e260f5e08c4816af7ba44dc60f69922a09
Denis Skibin <d.sckibin2017@yandex.ru>
2024-08-07 16:10:58 +0300

    Adding a builder to AddProfileRequest

--- src/main/java/org/example/taskmanager/api/request/profile/AddProfileRequest.java
+++ src/main/java/org/example/taskmanager/api/request/profile/AddProfileRequest.java
@@ -4,13 +4,16 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class AddProfileRequest {
 
     @Size(min = 1, max = 100, message = "Wrong profile name size")
+    @NotNull
     private String name;
     @Email
     @NotNull
